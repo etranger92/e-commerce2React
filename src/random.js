1ade7e0fae04c422b2c37944382cf461ca4ef41a
etranger92 <j.sba@outlook.com>
2020-01-16 01:36:56 +0000

    jj

--- src/random.js
+++
@@ -1,7 +0,0 @@
-const name = "nabil";
-const surname = "Amjade2409.";
-const pass = "sk_test_Q4G4XO257sfFTvhv4ob5DQeO";
-
-module.exports.name = name;
-module.exports.surname = surname;
-module.exports.pass = pass;
